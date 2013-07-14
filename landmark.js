5983a772636c27e093b43c79bd3a1c15ae90cca6
Ben Johnson <benbjohnson@yahoo.com>
2013-07-13 21:57:04 -0600

    Add UUID v4 generator.

--- landmark.js
+++ landmark.js
@@ -302,7 +302,20 @@
     return null;
   }
 
-  landmark.__test__ = {setCookie: setCookie, getCookie: getCookie};
+  /**
+   * Generates an RFC4122 version 4 UUID.
+   *
+   * http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+   */
+  function uuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+  }
+
+  landmark.__test__ = {setCookie: setCookie, getCookie: getCookie, uuid:uuid};
+
 
   //--------------------------------------------------------------------------
   //
