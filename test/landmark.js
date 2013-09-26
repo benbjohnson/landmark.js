ed98c7ed6c21f940da2f35960e57f23ceca29740
Ben Johnson <benbjohnson@yahoo.com>
2013-09-25 21:47:40 -0600

    Add identify().

--- test/landmark.js
+++ test/landmark.js
@@ -10,10 +10,40 @@ describe('Landmark', function(){
     });
 
     it('should set cookie options', function(){
-      landmark.initialize({cookie: {maxage: 100 }});
+      landmark.initialize({cookie: {maxage: 100}});
       assert(landmark.cookie.options().maxage == 100);
     });
 
-    it('should load the user')
+    it('should set user options', function(){
+      landmark.initialize({user: {foo: "bar"}});
+      assert(landmark.user.options().foo == "bar");
+    });
+  });
+
+  describe('#identify', function () {
+    beforeEach(function() {
+      landmark.cookie.set("ldmk_user_id", null);
+    });
+
+    it('should set the user id only', function(){
+      landmark.identify("foo");
+      assert(landmark.user.id() == "foo");
+    });
+
+    it('should set the user id and traits', function(){
+      landmark.identify("foo", {"name":"bob"});
+      assert(landmark.user.id() == "foo");
+      assert(landmark.user.traits().name == "bob");
+    });
+
+    it('should set the traits only', function(){
+      landmark.identify({"name":"susy"});
+      assert(landmark.user.id() == null);
+      assert(landmark.user.traits().name == "susy");
+    });
+
+    it('should invoke callback', function(done){
+      landmark.identify("foo", {"name":"john"}, done);
+    });
   });
 });
