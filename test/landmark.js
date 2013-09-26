c8bff4945bc86378fcd64a0d9a77137cb060e5c1
Ben Johnson <benbjohnson@yahoo.com>
2013-09-25 20:43:44 -0600

    Add cookies.

--- test/landmark.js
+++ test/landmark.js
@@ -6,7 +6,14 @@ describe('Landmark', function(){
   describe('#initialize()', function(){
     it('should be initialized', function(){
       landmark.initialize();
-      assert(landmark._initialized);
-    })
-  })
-})
+      assert(landmark.initialized == true);
+    });
+
+    it('should set cookie options', function(){
+      landmark.initialize({cookie: {maxage: 100 }});
+      assert(landmark.cookie.options().maxage == 100);
+    });
+
+    it('should load the user')
+  });
+});
